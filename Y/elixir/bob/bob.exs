4f8ac7313401dadde4f53a2e1484b617e0f26e5b
Leo Correa <lcorr005@gmail.com>
2013-07-24 00:56:32 -0400

    Use critique received from Ruby code

--- Y/elixir/bob/bob.exs
+++ Y/elixir/bob/bob.exs
@@ -1,10 +1,22 @@
 defmodule Teenager do
   def hey(message) do
     cond do
-      String.length(message) == 0 -> "Fine. Be that way."
-      String.upcase(message) == message ->  "Woah, chill out!"
-      String.ends_with?(message, "?") -> "Sure."
+      Message.silent?(message)   -> "Fine. Be that way."
+      Message.loud?(message)     ->  "Woah, chill out!"
+      Message.question?(message) -> "Sure."
       true ->  "Whatever."
     end
+  end
+end
+
+defmodule Message do
+  def silent?(message) do
+    String.length(message) == 0
+  end
+  def loud?(message) do
+    String.upcase(message) == message
+  end
+  def question?(message) do
+    String.ends_with?(message, "?")
   end
 end
