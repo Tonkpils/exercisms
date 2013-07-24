ae0d6a0873288d7d4a8bb9bd5f028721d3376dc0
Leo Correa <lcorr005@gmail.com>
2013-07-24 01:01:29 -0400

    Implement critique on Message class

--- Y/ruby/bob/bob.rb
+++ Y/ruby/bob/bob.rb
@@ -1,23 +1,20 @@
 class Bob
   def hey(msg)
+    msg = Message.new(msg)
     case
-    when silence?(msg)  then "Fine. Be that way."
-    when loud?(msg)     then "Woah, chill out!"
-    when question?(msg) then "Sure."
+    when msg.silent?   then "Fine. Be that way."
+    when msg.loud?     then "Woah, chill out!"
+    when msg.question? then "Sure."
     else "Whatever."
     end
   end
-
-  def silence?(msg)
-    msg.nil? || msg.empty?
-  end
-
-  def loud?(msg)
-    msg.upcase == msg
-  end
-
-  def question?(msg)
-    msg.end_with?("?")
-  end
 end
 
+class Message
+  attr_reader :message
+  def initialize(msg); @message = msg end
+ 
+  def silent?; @message.nil? || @message.empty? end
+  def loud?;   @message.upcase == @message end
+  def question?; @message.end_with?("?") end
+end
