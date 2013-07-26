5f06c02c8aaf5f83e893f64777415ee3cee3a6a2
Leo Correa <lcorr005@gmail.com>
2013-07-26 10:08:02 -0400

    Change to use Regex.scan and switch regex to need + word or decimal

--- Y/elixir/word-count/word-count.exs
+++ Y/elixir/word-count/word-count.exs
@@ -3,14 +3,11 @@ defmodule Words do
     string |> sanitize |> aggregate
   end
 
-  defp sanitize(string), do: Regex.split(%r/[^a-z0-9]/, String.downcase(string))
+  defp sanitize(string), do: Regex.scan(%r/[a-z0-9]+/, String.downcase(string))
   defp aggregate(word_list) do
     Enum.reduce word_list, HashDict.new, fn(word, acc) ->
-      cond do
-        word == "" -> acc
-        true -> HashDict.merge acc, [{word, 1}], fn(_word, count, word_value) ->
+      HashDict.merge acc, [{word, 1}], fn(_word, count, word_value) ->
           count + word_value
-        end
       end
     end
   end
