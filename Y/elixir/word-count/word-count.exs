b59be08790dc59d956373b43da42513b9c65737c
Leo Correa <lcorr005@gmail.com>
2013-07-26 12:45:30 -0400

    Separate downcase to use pipeline operator

--- Y/elixir/word-count/word-count.exs
+++ Y/elixir/word-count/word-count.exs
@@ -1,9 +1,10 @@
 defmodule Words do
   def count(string) do
-    string |> sanitize |> aggregate
+    string |> downcase |> sanitize |> aggregate
   end
 
-  defp sanitize(string), do: Regex.scan(%r/[a-z0-9]+/, String.downcase(string))
+  defp downcase(string), do: String.downcase(string)
+  defp sanitize(string), do: Regex.scan(%r/[a-z0-9]+/, string)
   defp aggregate(word_list) do
     Enum.reduce word_list, HashDict.new, fn(word, acc) ->
       HashDict.merge acc, [{word, 1}], fn(_word, count, word_value) ->
