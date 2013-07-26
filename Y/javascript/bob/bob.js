5affd2e1a11988907c3962dafe0d7444202e9f26
Leo Correa <lcorr005@gmail.com>
2013-07-25 20:48:25 -0400

    First crack at js

--- Y/javascript/bob/bob.js
+++ Y/javascript/bob/bob.js
@@ -1,2 +1,22 @@
-function Bob(){
-}
+function Bob() {
+  this.hey = function(message) {
+    if(message === "") 
+    {
+      return "Fine, be that way.";
+    }
+    else if(message.toUpperCase() === message)
+    {
+      return "Woah, chill out!";
+    } 
+    else if(message.match("\\?$") == "?") 
+    {
+      return "Sure";
+    } 
+    else 
+    {
+      return "Whatever" 
+    }
+  };
+};
+
+module.exports = Bob
