6f62c6a52df704b173edf3076fc08e4c2383a0ac
Leo Correa <lcorr005@gmail.com>
2013-07-27 01:14:13 -0400

    Split off conditions into private functions clojure

--- Y/clojure/bob/bob.clj
+++ Y/clojure/bob/bob.clj
@@ -1,9 +1,13 @@
 (ns bob
   (:use [clojure.string :only [blank? upper-case]]))
 
+(defn- silence? [msg] (blank? msg))
+(defn- query?   [msg] (.endsWith msg "?"))
+(defn- shout?   [msg] (= msg (upper-case msg)))
+
 (defn response-for [msg]
   (cond
-    (blank? msg) "Fine, be that way."
-    (.endsWith msg "?") "Sure."
-    (= (upper-case msg) msg) "Woah, chill out!"
+    (silence? msg) "Fine, be that way."
+    (query?   msg) "Sure."
+    (shout?   msg) "Woah, chill out!"
     :else "Whatever."))
