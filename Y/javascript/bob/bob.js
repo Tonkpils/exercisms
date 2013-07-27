b43f4eb069f0a48fca1d1a5baaf841584280d0f1
Leo Correa <lcorr005@gmail.com>
2013-07-27 13:47:28 -0400

    Use strict and IIFE

--- Y/javascript/bob/bob.js
+++ Y/javascript/bob/bob.js
@@ -1,27 +1,28 @@
-function Bob() {
+(function() {
+  'use strict';
+  function Bob() {
 
-  this.hey = function(message) {
-    if(isSilence(message)) {
-      return "Fine, be that way.";
-    } else if(isScream(message)) {
-      return "Woah, chill out!";
-    } else if(isQuestion(message)) {
-      return "Sure";
-    } else {
-      return "Whatever" 
-    }
-  };
+    this.hey = function(message) {
+      if(isSilence(message)) {
+        return "Fine, be that way.";
+      } else if(isScream(message)) {
+        return "Woah, chill out!";
+      } else if(isQuestion(message)) {
+        return "Sure";
+      } else {
+        return "Whatever" 
+      }
+    };
 
-  var isSilence = function(message){
-    return (message === "");
+    var isSilence = function(message){
+      return (message === "");
+    };
+    var isQuestion = function(message){
+      return (message.slice(-1) == "?");
+    };
+    var isScream = function(message){
+      return (message.toUpperCase() === message);
+    };
   };
-  var isQuestion = function(message){
-    return (message.slice(-1) == "?");
-  };
-  var isScream = function(message){
-    return (message.toUpperCase() === message);
-  };
-
-};
-
-module.exports = Bob
+  module.exports = Bob
+}());
