0ac510feeced6103a6b5835495ad452738907a3e
Leo Correa <lcorr005@gmail.com>
2013-08-01 20:48:02 -0400

    Fix up elixir to use shorthand for fn and \w

--- Y/elixir/word-count/word-count.exs
+++ Y/elixir/word-count/word-count.exs
@@ -4,11 +4,11 @@ defmodule Words do
   end
 
   defp downcase(string), do: String.downcase(string)
-  defp sanitize(string), do: Regex.scan(%r/[a-z0-9]+/, string)
+  defp sanitize(string), do: Regex.scan(%r/[\w]+/, string)
   defp aggregate(word_list) do
     Enum.reduce word_list, HashDict.new, fn(word, accumulator) -> 
       accumulator |> update_dictionary word 
     end  
   end
-  defp update_dictionary(dictionary, word), do: HashDict.update(dictionary, word, 1, fn(value) -> value + 1 end)
+  defp update_dictionary(dictionary, word), do: HashDict.update(dictionary, word, 1, &1 + 1)
 end
