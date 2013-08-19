8e12f2537665adecba8aefa6e82e0d739b0fc8ba
Leo Correa <lcorr005@gmail.com>
2013-08-18 22:34:57 -0400

    Regex scan returns a list of lists in elixir 0.10.1

--- Y/elixir/word-count/word-count.exs
+++ Y/elixir/word-count/word-count.exs
@@ -4,7 +4,7 @@ defmodule Words do
   end
 
   defp downcase(string), do: String.downcase(string)
-  defp split_words(string), do: Regex.scan(%r/\w+/, string)
+  defp split_words(string), do: Regex.scan(%r/\w+/, string) |> List.flatten
   defp aggregate(word_list) do
     Enum.reduce word_list, HashDict.new, fn(word, accumulator) -> 
       accumulator |> update_dictionary word 
