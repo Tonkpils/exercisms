c587aef93eff9e21e32d543e66f79d5721ad33e1
Leo Correa <lcorr005@gmail.com>
2013-08-30 15:12:51 -0400

    Use a module function to count the hamming distance

--- Y/ruby/point-mutations/dna.rb
+++ Y/ruby/point-mutations/dna.rb
@@ -7,18 +7,20 @@ class DNA
 
   def hamming_distance(dna_strand)
     foreign_strand = Strand.new(dna_strand)
-    strand.diff_count(foreign_strand)
+    HammingDistance.count(strand, foreign_strand)
+  end
+end
+
+module HammingDistance
+  def self.count(strand, other_strand)
+    strand.nucleotides.zip(other_strand.nucleotides).count do |original, foreign|
+      foreign && original != foreign
+    end
   end
 end
 
 Strand = Struct.new(:sequence) do
   def nucleotides
     sequence.chars
-  end
-
-  def diff_count(another_strand)
-    nucleotides.zip(another_strand.nucleotides).count do |original, foreign|
-      foreign && original != foreign
-    end
   end
 end
